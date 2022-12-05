5b1d073555e7164ad8478eb21174c78a42c3dea6
GK <gkarcevski@gmail.com>
2022-12-05 12:42:55 -0800

    Update Shap Values.py

--- Shap Values.py
+++ Shap Values.py
@@ -5,7 +5,7 @@
 
 # #### Import Modules
 
-# In[33]:
+# In[1]:
 
 
 import pandas as pd
@@ -24,7 +24,7 @@ import pickle
 
 # #### Import our Feature Table
 
-# In[43]:
+# In[2]:
 
 
 #load preprocessed data
@@ -35,7 +35,7 @@ stocks_df1 = stocks_df1.drop(columns = ["Unnamed: 0"])
 # ___
 # #### Create Clean Functions for Viewing SHAP 
 
-# In[40]:
+# In[3]:
 
 
 def preprocess_modeling_data(df, stock, modeling_days):
@@ -65,7 +65,7 @@ def preprocess_modeling_data(df, stock, modeling_days):
     return X_train, X_test, y_train, y_test
 
 
-# In[41]:
+# In[21]:
 
 
 def display_shap_barplot(df, stock, modeling_days, model, X_train):
@@ -98,7 +98,37 @@ def display_shap_barplot(df, stock, modeling_days, model, X_train):
     shap.plots.bar(shap_values, max_display = 15)
 
 
-# In[42]:
+# In[64]:
+
+
+def display_actual_vs_predicted(df, stock, modeling_days, model, X_train):
+    """
+    use this to display our shap bar plot
+    """
+    
+    gk_stock = stock
+
+    #use this section to pick/change/adjust the features
+    feature_list = ["Sector_enc", "return_lag1", "return_rolling5", 'Volume_lag1', 
+                    "stock_VIX", "sector_VIX", "moving_avg_50d", "moving_avg_200d", 'open_close_over_high_low_ratio_lag1',
+                    "exp_moving_avg_50d", "exp_moving_avg_200d", 'percent_daily_volume_total_market_lag1']
+
+    recent_date = df.loc[df.index[modeling_days],'Date']
+    
+   #input for only this stock
+    this_data = df[np.logical_and(df['Date'] == recent_date,
+                                  df['Symbol'] == gk_stock)][feature_list]
+
+    this_actual = df[np.logical_and(df['Date'] == recent_date, 
+                                    df['Symbol'] == gk_stock)]['return']
+    
+    prediction = xgb.predict(this_data)
+    
+    print(f"""Prediction is: {prediction}
+Actual is: {this_actual.values}""")
+
+
+# In[66]:
 
 
 #View Our SHAP Plots below
@@ -106,36 +136,39 @@ def display_shap_barplot(df, stock, modeling_days, model, X_train):
 for day in list(range(1, 11, 1)):
     xgb = pickle.load(open(f'model_day{day}.pkl', 'rb'))
     print(f"Model Day {day}")
+    display_actual_vs_predicted(stocks_df1, "AAPL", -(day), xgb, X_train)
     X_train, X_test, y_train, y_test = preprocess_modeling_data(stocks_df1, "AAPL", -(day))
     print(f"Model Day {day} Importances")
     display_shap_barplot(stocks_df1, "AAPL", -(day), xgb, X_train)
 
 
-# In[ ]:
-
-
-
-
-
 # ___
 # Flask below
 
-# In[28]:
+# In[16]:
 
 
 from flask import Flask, request
 
 
-# In[29]:
+# In[34]:
 
 
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
-    
 app = Flask(__name__)
 
+@app.route("/get_shap")
+#View Our SHAP Plots below
+def show_shap():
+    for day in list(range(1, 11, 1)):
+        with open(f"model_day{day}.pkl", "rb") as f:
+            xgb = pickle.load(f)
+            print(f"Model Day {day}")
+            X_train, X_test, y_train, y_test = preprocess_modeling_data(stocks_df1, "AAPL", -(day))
+            print(f"Model Day {day} Importances")
+            display_shap_barplot(stocks_df1, "AAPL", -(day), xgb, X_train)
 
-# In[32]:
+
+# In[33]:
 
 
 if __name__ == "__main__":
